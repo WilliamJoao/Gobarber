c49b7954a42467bbe1441627c6a45e3623cc8029
William Vieira <william.joao@yahoo.com.br>
2019-08-29 16:50:09 -0300

    Criação da Controller de apontamentos

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,9 +19,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
-
 routes.get('/providers', ProviderController.index);
-
+routes.post('/appoitments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
