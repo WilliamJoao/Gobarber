9dc299a0416d164cdcebeaacea5d7f42cbfdf9e7
William Vieira <william.joao@yahoo.com.br>
2019-08-30 16:15:49 -0300

    Listando Agenda de prestadores

--- src/routes.js
+++ src/routes.js
@@ -6,9 +6,10 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
-import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,6 +23,7 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.get('/appoitments', AppointmentController.index);
 routes.post('/appoitments', AppointmentController.store);
+routes.get('/schedule', ScheduleController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
