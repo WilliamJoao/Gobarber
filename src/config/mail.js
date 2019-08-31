7077905e468f773f170479e8b895ba885ac7ad2f
William Vieira <william.joao@yahoo.com.br>
2019-08-31 11:14:56 -0300

    Configurando nodemailer e configurando templates de email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: '4e4e430b88f3cc',
+        pass: '7ba4121965d5e9',
+    },
+    default: {
+        from: 'Equipe GoBarber <noreply@gobarber.com>',
+    },
+};
