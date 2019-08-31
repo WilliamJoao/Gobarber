e2499d6fcd1c104dcc8135c38d5a450eb9730ece
William Vieira <william.joao@yahoo.com.br>
2019-08-31 09:30:40 -0300

    Cancelamento de agendamento

--- src/routes.js
+++ src/routes.js
@@ -21,12 +21,18 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
 routes.get('/appoitments', AppointmentController.index);
 routes.post('/appoitments', AppointmentController.store);
+routes.delete('/appoitments/:id', AppointmentController.delete);
+
 routes.get('/schedule', ScheduleController.index);
+
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
