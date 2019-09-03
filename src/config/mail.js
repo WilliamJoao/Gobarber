81df6bbf2c35dfbb39ef5444136db71cb4c84800
William Vieira <william.joao@yahoo.com.br>
2019-09-03 16:13:45 -0300

    Tratamento de exceções e configurações de variaveis de ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-    host: 'smtp.mailtrap.io',
-    port: 2525,
+    host: process.env.MAIL_HOST,
+    port: process.env.MAIL_PORT,
     secure: false,
     auth: {
-        user: '4e4e430b88f3cc',
-        pass: '7ba4121965d5e9',
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
     },
     default: {
         from: 'Equipe GoBarber <noreply@gobarber.com>',
